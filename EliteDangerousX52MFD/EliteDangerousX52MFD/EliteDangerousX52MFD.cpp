b504e9b5ddec2bfda951991024491d02f048cb33
Anthony Zaprzalka <azmail@comcast.net>
2016-09-17 21:35:29 -0500

    Able to communicate with device and set a basic page and strings to MFD

--- EliteDangerousX52MFD/EliteDangerousX52MFD/EliteDangerousX52MFD.cpp
+++ EliteDangerousX52MFD/EliteDangerousX52MFD/EliteDangerousX52MFD.cpp
@@ -5,38 +5,70 @@
 #undef max
 
 using namespace std;
-HRESULT hr;
 
+// DirectOutput function object
+// Creation of this object automatically loads in DirectOutput but it still needs to be initialized
+DirectOutputFn fn;
+// Filepath of the profile to use
+wchar_t* profileFilepath = TEXT("C:\\Users\\Public\\Documents\\SmartTechnology Profiles\\Elite Dangerous.pr0\0");
+
+void checkHR(HRESULT hr);
 
 int main()
 {
-	// Creating an object of the class automatically loads in the DirectOutput dll
-	DirectOutputFn fn;
+	// Initialize DirectOutput
+	checkHR(fn.Initialize(L"EliteDangerousX52MFD"));
 
-	hr = fn.Initialize(L"EliteDangerousX52MFD");
+	// Registers and enumerates device
+	fn.RegisterDevice();
+
+	// Gets the enumerated device
+	fn.GetDeviceType();
+
+	// Set the profile
+	checkHR(fn.setDeviceProfile(profileFilepath));
+
+	// Set greeting message
+	// Page 0 (initial page) will be reserved for greeting and profile name
+	// Add/Set page
+	checkHR(fn.setPage(0, FLAG_SET_AS_ACTIVE));
+
+	// Set String
+	checkHR(fn.setString(0, 0, TEXT("Greetings CMDR")));
+	checkHR(fn.setString(0, 2, TEXT("Elite Dangerous")));
+
+	// Pause to check outputs
+	cout << "Press enter to deinitialize.\n";
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	if (hr == S_OK)
-	{
-		cout << "DONE.\n";
-	}
-	else
-	{
-		cout << "FAILED.\n";
-	}
 
-	hr = fn.Deinitialize();
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	if (hr == S_OK)
-	{
-		cout << "DONE.\n";
-	}
-	else
-	{
-		cout << "FAILED.\n";
-	}
+	// Deinitialize DirectOutput
+	checkHR(fn.Deinitialize());
 
-	
+
+	// TEMP -> Create some runtime 
+	// PAUSE TO CHECK STATUSES 
+	cout << "Press enter to free DirectOutput.dll and quit.\n";
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
     return 0;
 }
 
+/*
+	INPUTS: HRESULT hr == some functions from DirectOutput return a HRESULT value, this just checks if it pass/fail and ouputs to the console
+	RETURNS: none
+
+	FUNCTION: Checks resulkt of the function if it returns an HRESULT value
+
+	Read more about HRESULT return types at
+	Microsoft (MSDN) -> https://msdn.microsoft.com/en-us/library/windows/desktop/aa378137(v=vs.85).aspx
+	Wikipedia -> https://en.wikipedia.org/wiki/HRESULT
+*/
+void checkHR(HRESULT hr) {
+	if (hr == S_OK)
+	{
+		cout << "DONE.\n";
+	}
+	else
+	{
+		cout << "FAILED.\n";
+	}
+}
